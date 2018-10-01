3013e8da2dac3902babb6374d6d4d4a0baca0719
Nik Torfs <niktorfs@gmail.com>
2018-10-01 11:49:33 +0200

    Module5

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,10 @@ class Quiz extends Component {
     this.state = {quiz_position:1}
   }
   
+  showNextQuestion () {
+    this.setState ({quiz_position: this.state.quiz_position + 1})
+  }
+
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length ;
     return (
@@ -18,7 +22,8 @@ class Quiz extends Component {
         { isQuizEnd && <QuizEnd /> }
         { !isQuizEnd && 
             <QuizQuestion 
-              quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
+              quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+              showNextQuestionHandler={this.showNextQuestion.bind(this)} /> }
       </div>
       
     )
